210c805591ee6966e89c8b871c91684d7f698c87
jacob <jakechap08@gmail.com>
2018-06-20 11:14:02 -0400

    second part of test function

--- test_function.rb
+++ test_function.rb
@@ -1,15 +1,38 @@
+# def doopy(a, b)
+# 	if a == b
+# 		puts "that is correct"
+# 	elsif 
+# 	    puts "wrong mofo" 	 
+#     end
+# end
+
+# doopy(1, 2)
+# doopy(2, 2)
+# doopy(21, 21)
+# doopy(300, 300)
+# doopy(2,100)
+# end
+
 def doopy(a, b)
-	a == 2 ; b == 2
-	if a == b
-		puts "that is correct"
-	elsif 
-	    puts "wrong mofo" 	 
-    end
+   if compare_class(a, b) == false
+   	return false
+   end
+   if a ==b 
+   	return true
+   end
+   false
 end
 
-doopy(1, 2)
-doopy(2, 2)
-doopy(21, 21)
-doopy(300, 300)
-doopy(2,100)
-			
+def compare_class(a, b)
+	if a.class == b.class
+	 return true
+    else
+	return false
+end
+
+p doopy(5, "5")
+p doopy("10", "10")
+p doopy([10], 10)
+p doopy({10}, {10})
+
+
