07790bc3dcaf20329b47d15c986acbb7429f70a6
jacob <jakechap08@gmail.com>
2018-06-20 10:10:02 -0400

    first commit

---
+++ test_function.rb
@@ -0,0 +1,15 @@
+def doopy(a, b)
+	a == 2 ; b == 2
+	if a == b
+		puts "that is correct"
+	elsif 
+	    puts "wrong mofo" 	 
+    end
+end
+
+doopy(1, 2)
+doopy(2, 2)
+doopy(21, 21)
+doopy(300, 300)
+doopy(2,100)
+			
